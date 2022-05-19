1fe4ce518765f43f02874582b15148b0dd83eb13
Marlon <marlon14br@gmail.com>
2022-05-19 13:01:12 -0300

    comentando o codigo JS

--- script.js
+++ script.js
@@ -3,25 +3,36 @@ var botaoCifraDecod = document.getElementById("decodCesar")//VARIÁVEL CRIADA PA
 botaoCifra.addEventListener("click", conversaoCesar)//ADICIONADO UM EVENTO AO BOTÃO JUNTO DE UMA FUNÇÃO PARA
 // SER EXECUTADA AO CLICAR. 
 
-botaoCifraDecod.addEventListener("click",DecodificacaoCesar)
+botaoCifraDecod.addEventListener("click",DecodificacaoCesar) // ADICIONADO UM EVENTO DE CLICK E UMA FUNÇÃO
+// AO DECODIFICADOR PARA QUANDO CLICADO A FUNÇÃO SER ACIONADA.
  
 function conversaoCesar(){
     var pegaEntrada = document.getElementById("entradaText")//VAR QUE PEGA A PRIMEIRA CAIXA DE TEXTO NO HTML.
     var pegaSaida = document.getElementById("saidaText")//VAR QUE PEGA A SEGUNDA CAIXA DE TEXTO NO HTML.
     var deslocamento = Number(document.getElementById("desloc").value)//VAR QUE PEGA O INPUT ONDE É PASSADO O Nº DE DESLOCAMENTO.
-    var textoEntrada = pegaEntrada.value
-    var textoSaida = ""
+    var textoEntrada = pegaEntrada.value //AQUI ELE PEGA O VALOR QUE FOR PASSADO NA PRIMEIRA CAIXA E GUARDA.
+    var textoSaida = "" // AQUI É UMA SAÍDA VAZIA. ( POR ENQUANTO.) 
 
-    for(i = 0; i < textoEntrada.length; i++){
-        var conversao = textoEntrada.charCodeAt(i) + deslocamento
-        var charcodeToString = String.fromCharCode(conversao);
-        textoSaida += charcodeToString
+    for(i = 0; i < textoEntrada.length; i++){ // REPETIÇÃO PARA PASSAR LETRA POR LETRA PEGANDO SEU CHARCODE.
+        var conversao = textoEntrada.charCodeAt(i) + deslocamento// AQUI VOCÊ TEM UMA NOVA VARIÁVEL QUE 
+        // VAI PEGAR O VALOR QUE FOR PASSADO NO TEXTO DE ENTRADA E VAI SOMAR COM O DESLOCAMENTO// POR EXEMPLO
+        // A LETRA " A " NO CHARCODE É 65, OU SEJA + A POSIÇÃO SERÁ ASSIM 65 + 5 = 70.
+
+        var charcodeToString = String.fromCharCode(conversao); // AQUI ELE PEGA O VALOR EM CHARCODE E TRANSFORMA 
+        // EM STRING NOVAMENTE. EXEMPLO SE O RESULTADO FOSSE = 65, LOGO ELE SE TORNA "A"
+
+        textoSaida += charcodeToString // AQUI ELE VAI PEGAR O TEXTO SAÍDA QUE ESTAVA VAZIO E VAI GUARDAR O RESULTADO DA CONVERSÃO.
+        //
     }
  
-        pegaSaida.innerText = textoSaida
+        pegaSaida.innerText = textoSaida // AQUI ELE IRÁ PEGAR O VALOR QUE FOI GUARDADO NO TEXTO SAÍDA
+        // E IRÁ COLOCAR DENTRO DO TEXTO INTERNO DA CAIXA DE SAIDA, OU SEJA APÓS TODA A CONVERSÃO ELE 
+        // SÓ ESTÁ REPASSANDO A RESPOSTA PARA CAIXA 2.
 
 }
 
+// A FUNÇÃO DECODIFICAR RECEBE EXATAMENTE TODAS  AS INSTRUÇÕES COM EXCESSÃO DE QUÊ, AO INVÉS DE VOCÊ
+// COLOCAR POSIÇÃO + DESLOCAMENTO, VOCÊ IRÁ COLOCAR POSIÇÃO - DESLOCAMENTO. 
 function DecodificacaoCesar(){
     var pegaEntrada = document.getElementById("entradaText")
     var pegaSaida = document.getElementById("saidaText")
